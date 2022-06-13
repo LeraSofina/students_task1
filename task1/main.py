1a8dd844d1655574139e324929b0870a60a1323a
Валерия Софьина <valeriiasofina@gmail.com>
2022-06-13 15:38:47 +0500

    task2

--- task1/main.py
+++ task1/main.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lect(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -16,12 +25,18 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname, ):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+
+    def lecturer_grade(self, grade):
+        self.grade = {}
 
 
 class Reviewer(Mentor):
     pass
-
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -36,7 +51,7 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
@@ -44,3 +59,15 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+good_student = Student('Anybody', 'Somebody', 'male')
+good_student.courses_in_progress += ['Python']
+
+best_lecturer = Lecturer('First', 'Second')
+best_lecturer.courses_attached += ['Python']
+
+good_student.rate_lect(best_lecturer, 'Python', 8)
+good_student.rate_lect(best_lecturer, 'Python', 9)
+good_student.rate_lect(best_lecturer, 'Python', 10)
+
+print(best_lecturer.grades)
